7ade8aaf43edc1b7a747ffe93ec6d7a5509d7f7a
Stephen Rumpp (RIT Student) <sar7743@rit.edu>
2022-02-25 19:03:40 -0500

    pushing for safety

--- portfolio/src/main.js
+++ portfolio/src/main.js
@@ -1,8 +1,6 @@
 function setDropDown(){
     const burgerIcon = document.querySelector('#burger');
-    const navbarMenu = document.querySelector('#menu-links');
-  
   burgerIcon.addEventListener("click", () => {
-      navbarMenu.classList.toggle('is-active');
+      
   });
   }
